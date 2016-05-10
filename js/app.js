f510c5df02423be3a562f22657448e919c51cf85
Ashish Kumar Tewari <ashishtewaripro@gmail.com>
2016-05-10 23:19:35 +0530

    initial

---
+++ js/app.js
@@ -0,0 +1,30 @@
+angular.module('fontly', []).controller("fontCalculator", function(){
+  this.font = 0;
+  this.breakpoints = [
+    {
+      pixel: 320
+    },
+    {
+      pixel: 480
+    },
+    {
+      pixel: 768
+    },
+    {
+      pixel: 992
+    },
+    {
+      pixel: 1170
+    },
+    {
+      pixel: 1200
+    }
+  ]
+
+this.add = function(newbreakpoint){
+  console.log("adding breakpoint: " + newbreakpoint + " px");
+  var breakpoint = {pixel:newbreakpoint};
+  this.breakpoints.push(breakpoint);
+  breakpoint= {};
+};
+});
